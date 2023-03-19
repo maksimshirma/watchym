b4e4cdc771fc0d6a8f0d969b0c05261af69490c2
maksim shirma <maksimshirma@mail.ru>
2023-03-19 20:21:36 +0300

    add production

--- server/app.js
+++ server/app.js
@@ -3,8 +3,10 @@ import mongoose, { connect } from "mongoose";
 import config from "config";
 import chalk from "chalk";
 import cors from "cors";
+import path from "path";
 import { initDatabase } from "./startUp/initDatabase.js";
 import routes from "./routes/index.js";
+import {fileURLToPath} from 'url';
 
 const app = express();
 
@@ -14,6 +16,16 @@ app.use(cors());
 app.use("/api", routes);
 
 const PORT = config.get("port") ?? 8080;
+
+if (process.env.NODE_ENV === "production") {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    app.use("/", express.static(path.join(__dirname, "client")));
+    const indexPath = path.join(__dirname, "client", "index.html");
+    app.get("*", (req, res) => {
+        res.sendFile(indexPath);
+    })
+}
 
 async function start() {
     try {
